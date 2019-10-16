1be091591304586576f45d1053f80ecad1052909
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-16 09:15:22 +0000

    Snapshot of branch master taken on 2019-10-16T05:15:20.913-04:00 from "https://github.com/UnofficialJuliaMirror/AsterReader.jl-cb1a753d-6b7b-52e3-9bfe-57d99d445c39"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,11 @@
 # This file is a part of JuliaFEM.
 # License is MIT: see https://github.com/JuliaFEM/AsterReader.jl/blob/master/LICENSE
 
-using FEMBase, AsterReader, Test, LinearAlgebra, SparseArrays
-
-include(joinpath("..", "docs", "make.jl"))
+using Test, AsterReader
 
 @testset "AsterReader.jl" begin
     include("test_read_aster_mesh.jl")
     include("test_read_aster_results.jl")
     include("test_read_gmsh_med.jl")
+    include("test_read_file_not_found.jl")
 end
-
-include(joinpath("..", "docs", "deploy.jl"))
